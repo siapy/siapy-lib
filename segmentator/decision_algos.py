bd030fd9e7ce5540e3d0bdc2638a126f35afdbce
Janez Lapajne <lapajne.janez@gmail.com>
2022-08-09 11:14:29 +0200

    changed cython with numba

--- segmentator/decision_algos.py
+++ segmentator/decision_algos.py
@@ -1,10 +1,11 @@
 import numpy as np
+from numba import njit, prange
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import SVC
 
 from segmentator.base_decision_algo import BaseDecisionAlgo
-from segmentator.formulas import sid
+
 
 
 class Lda(BaseDecisionAlgo):
@@ -46,6 +47,7 @@ class Sid(BaseDecisionAlgo):
         self.background_sig = None
         self.sid_threshold = None
 
+
     def _fit(self, X, y):
         y_inv = self.encoder.inverse_transform(y)
         X_remove = X[y_inv == self.cls_remove]
@@ -53,8 +55,8 @@ class Sid(BaseDecisionAlgo):
 
         self.background_sig = np.mean(X_remove, axis=0)
         # calculate sid for all extracted signatures
-        sid_remove = list(map(lambda sig: sid(sig, self.background_sig), X_remove))
-        sid_keep = list(map(lambda sig: sid(sig, self.background_sig), X_keep))
+        sid_remove = list(map(lambda sig: self.sid(sig, self.background_sig), X_remove))
+        sid_keep = list(map(lambda sig: self.sid(sig, self.background_sig), X_keep))
 
         # make column vecotrs
         sid_keep = np.atleast_2d(sid_keep).T
@@ -71,8 +73,23 @@ class Sid(BaseDecisionAlgo):
     def _predict(self, X):
         self.cls_keep_enc = self.encoder.transform([self.cls_keep])[0]
         self.cls_remove_enc = self.encoder.transform([self.cls_remove])[0]
-        targets = np.array([sid(self.background_sig, sig) for sig in X])
+        targets = np.array(self.sid_multi(np.array(X), self.background_sig, self.sid))
         # replace values above threshold with class keep and below with class remove
         targets = np.where(targets > self.sid_threshold, self.cls_keep_enc, self.cls_remove_enc)[0]
-
         return targets
+
+    @staticmethod    
+    @njit()
+    def sid(p, q):
+        p = p + np.spacing(1)
+        q = q + np.spacing(1)
+        return np.sum(p * np.log(p / q) + q * np.log(q / p))
+
+    @staticmethod    
+    @njit(parallel=True)
+    def sid_multi(X, background_sig, sid_func):
+        n_rows, _ = X.shape 
+        sid_values = np.zeros(n_rows)
+        for idx in prange(n_rows):
+            sid_values[idx] = sid_func(X[idx,:], background_sig)
+        return sid_values
