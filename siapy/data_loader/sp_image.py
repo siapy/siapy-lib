db3702440fa6993e4a2705fafd88fa222ef3e9f8
janezlapajne <lapajne.janez@gmail.com>
2024-06-09 10:30:07 +0200

    Remove unused configuration files and scripts

--- siapy/data_loader/sp_image.py
+++ siapy/data_loader/sp_image.py
@@ -1,29 +1,50 @@
 import os
+from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
+import spectral as sp
 
 
-class SPImage():
-    def __init__(self, sp_file, config):
+@dataclass
+class SPImage:
+    def __init__(self, sp_file):
         self._sp_file = sp_file
-        self._cfg = config
 
     def __repr__(self):
         return repr(self._sp_file)
 
-    def to_display(self):
-        brightness = self._cfg.image_display_brightness
+    @classmethod
+    def envi_open(cls, hdr_path, img_path=None):
+        sp_file = sp.envi.open(file=hdr_path, image=img_path)
+        return cls(sp_file)
+
+    def _remove_nan(self, image, nan_value=0):
+        image_mask = np.bitwise_not(np.bool_(np.isnan(image).sum(axis=2)))
+        image[~image_mask] = nan_value
+        return image
+
+    def to_display(self, brightness=1):
         db = self.metadata["default bands"]
         db = list(map(int, db))
         image_3ch = self._sp_file.read_bands(db)
-        image_3ch[:, :, 0] = image_3ch[:, :, 0] / (image_3ch[:, :, 0].max() / 255.) * brightness
-        image_3ch[:, :, 1] = image_3ch[:, :, 1] / (image_3ch[:, :, 1].max() / 255.) * brightness
-        image_3ch[:, :, 2] = image_3ch[:, :, 2] / (image_3ch[:, :, 2].max() / 255.) * brightness
+        image_3ch = self._remove_nan(image_3ch, nan_value=0)
+        image_3ch[:, :, 0] = (
+            image_3ch[:, :, 0] / (image_3ch[:, :, 0].max() / 255.0) * brightness
+        )
+        image_3ch[:, :, 1] = (
+            image_3ch[:, :, 1] / (image_3ch[:, :, 1].max() / 255.0) * brightness
+        )
+        image_3ch[:, :, 2] = (
+            image_3ch[:, :, 2] / (image_3ch[:, :, 2].max() / 255.0) * brightness
+        )
         return image_3ch.astype("uint8")
 
-    def to_numpy(self):
-        return self._sp_file[:,:,:]
+    def to_numpy(self, nan_value=None):
+        image = self._sp_file[:, :, :]
+        if nan_value is not None:
+            image = self._remove_nan(image, nan_value)
+        return image
 
     def to_signatures(self, pixels_loc):
         image_arr = self.to_numpy()
@@ -38,10 +59,6 @@ class SPImage():
     @property
     def file(self):
         return self._sp_file
-
-    @property
-    def config(self):
-        return self._cfg.copy()
 
     @property
     def metadata(self):
@@ -69,10 +86,6 @@ class SPImage():
     @property
     def filename(self):
         return self._sp_file.filename.split(os.sep)[-1].split(".")[0]
-
-    @property
-    def camera_name(self):
-        return self._cfg.name
 
     @property
     def wavelengths(self):
