f8e2c6cc7f6cb7bc2e32de5f97bf574be0735f3e
janezlapajne <lapajne.janez@gmail.com>
2024-06-09 17:07:05 +0200

    chore: Update dependencies and configuration files

--- siapy/data_loader/sp_image.py
+++ siapy/data_loader/sp_image.py
@@ -1,30 +1,40 @@
 import os
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
 
 import numpy as np
 import pandas as pd
 import spectral as sp
+from pydantic import validate_call
 
 
 @dataclass
 class SPImage:
-    def __init__(self, sp_file):
+    def __init__(
+        self,
+        sp_file: sp.io.envi.BilFile | sp.io.envi.BipFile | sp.io.envi.BsqFile,
+    ):
         self._sp_file = sp_file
 
     def __repr__(self):
         return repr(self._sp_file)
 
     @classmethod
-    def envi_open(cls, hdr_path, img_path=None):
+    def envi_open(
+        cls, hdr_path: str | Path, img_path: str | Path | None = None
+    ) -> "SPImage":
         sp_file = sp.envi.open(file=hdr_path, image=img_path)
+        if isinstance(sp_file, sp.io.envi.SpectralLibrary):
+            raise ValueError("Opened file of type SpectralLibrary")
         return cls(sp_file)
 
-    def _remove_nan(self, image, nan_value=0):
+    def _remove_nan(self, image: np.ndarray, nan_value: float = 0.0) -> np.ndarray:
         image_mask = np.bitwise_not(np.bool_(np.isnan(image).sum(axis=2)))
         image[~image_mask] = nan_value
         return image
 
-    def to_display(self, brightness=1):
+    def to_display(self, brightness: float = 1.0) -> np.ndarray:
         db = self.metadata["default bands"]
         db = list(map(int, db))
         image_3ch = self._sp_file.read_bands(db)
@@ -40,54 +50,66 @@ class SPImage:
         )
         return image_3ch.astype("uint8")
 
-    def to_numpy(self, nan_value=None):
+    def to_numpy(self, nan_value: float | None = None) -> np.ndarray:
         image = self._sp_file[:, :, :]
         if nan_value is not None:
             image = self._remove_nan(image, nan_value)
         return image
 
-    def to_signatures(self, pixels_loc):
-        image_arr = self.to_numpy()
-        signatures = image_arr[pixels_loc.y, pixels_loc.x, :]
-        data = {"x": pixels_loc.x, "y": pixels_loc.y, "signature": list(signatures)}
-        return pd.DataFrame(data, columns=["x", "y", "signature"])
+    @validate_call
+    def to_signatures(
+        self, pixel_coords: pd.DataFrame, append_to_input: bool = True
+    ) -> pd.DataFrame:
+        if "u" not in pixel_coords or "v" not in pixel_coords:
+            raise ValueError("Input DataFrame must contain 'u' and 'v' columns.")
+        u = pixel_coords.get("u")
+        v = pixel_coords.get("v")
 
-    def mean(self, axis=None):
+        image_arr = self.to_numpy()
+        signatures = list(image_arr[v, u, :])
+
+        data_out = pd.DataFrame(signatures, columns=["signature"])
+        if append_to_input:
+            data_out = pd.concat([pixel_coords, data_out], axis=1)
+
+        return data_out
+
+    def mean(self, axis: int | tuple[int] | None = None) -> float | np.ndarray:
         image_arr = self.to_numpy()
         return np.nanmean(image_arr, axis=axis)
 
     @property
-    def file(self):
+    def file(self) -> sp.io.envi.BilFile | sp.io.envi.BipFile | sp.io.envi.BsqFile:
         return self._sp_file
 
     @property
-    def metadata(self):
+    def metadata(self) -> dict[str, Any]:
         return self._sp_file.metadata
 
     @property
-    def shape(self):
+    def shape(self) -> tuple[int, int, int]:
         rows = self._sp_file.nrows
         samples = self._sp_file.ncols
         bands = self._sp_file.nbands
         return (rows, samples, bands)
 
     @property
-    def rows(self):
+    def rows(self) -> int:
         return self._sp_file.nrows
 
     @property
-    def cols(self):
+    def cols(self) -> int:
         return self._sp_file.ncols
 
     @property
-    def bands(self):
+    def bands(self) -> int:
         return self._sp_file.nbands
 
     @property
-    def filename(self):
+    def filename(self) -> str:
         return self._sp_file.filename.split(os.sep)[-1].split(".")[0]
 
     @property
-    def wavelengths(self):
-        wavelen = self._sp_file.metadata["wavelength"]
-        return list(map(float, wavelen))
+    def wavelengths(self) -> list[float]:
+        wavelength_data = self._sp_file.metadata["wavelength"]
+        return list(map(float, wavelength_data))
