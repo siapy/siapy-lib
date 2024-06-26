fix: Refactor code to use Path instead of ConvexHull for FreeDraw convex hull calculation, shapes tests added

siapy/entities/shapes.py
@@ -2,9 +2,10 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Literal
 
-from scipy.spatial import ConvexHull
+import numpy as np
+from matplotlib.path import Path
 
-from siapy.entities.pixels import Pixels
+from .pixels import Pixels
 
 
 @dataclass
@@ -67,7 +68,17 @@ class FreeDraw(Shape):
     def convex_hull(self) -> Pixels:
         if len(self.pixels) < 3:
             return self.pixels
+
         points = self.pixels.to_numpy()
-        hull = ConvexHull(points)
-        hull_points = points[hull.vertices]
-        return Pixels.from_iterable(hull_points.tolist())
+        points_path = Path(points)
+
+        # Create a grid of points that covers the convex hull area
+        u_min, v_min = points.min(axis=0)
+        u_max, v_max = points.max(axis=0)
+        u, v = np.meshgrid(np.arange(u_min, u_max + 1), np.arange(v_min, v_max + 1))
+        grid_points = np.vstack((u.flatten(), v.flatten())).T
+
+        # Filter points that are inside the convex hull
+        inside_points = grid_points[points_path.contains_points(grid_points)]
+
+        return Pixels.from_iterable(inside_points.tolist())

siapy/utils/plots.py
@@ -2,7 +2,6 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Polygon
 from matplotlib.path import Path
 from matplotlib.widgets import LassoSelector
 
@@ -77,7 +76,7 @@ def pixels_select_lasso(image: ImageType) -> list[Pixels]:
         logger.info("Selected.")
         nonlocal indices
         path = Path(vertices_selected)
-        indices = np.nonzero(path.contains_points(pixes_all_stack))[0]
+        indices = path.contains_points(pixes_all_stack)
 
     def onrelease(_):
         nonlocal indices, indices_list
@@ -115,17 +114,25 @@ def pixels_select_lasso(image: ImageType) -> list[Pixels]:
 
 def display_selected_areas(
     image: ImageType,
-    selected_areas: list[Pixels],
+    selected_areas: Pixels | list[Pixels],
     *,
     color: str = "red",
 ):
-    image_display = validate_image_to_numpy_3channels(image)
+    if not isinstance(selected_areas, list):
+        selected_areas = [selected_areas]
 
+    image_display = validate_image_to_numpy_3channels(image)
     fig, ax = plt.subplots()
     ax.imshow(image_display)
 
     for pixels in selected_areas:
-        polygon = Polygon(pixels.to_numpy(), color=color)
-        ax.add_patch(polygon)
+        ax.scatter(
+            pixels.u(),
+            pixels.v(),
+            lw=0,
+            marker="o",
+            c=color,
+            s=(72.0 / fig.dpi) ** 2,
+        )
 
     plt.show()