667725a91af2c84eebc7f4ed5be46cdd7213021a
janezlapajne <lapajne.janez@gmail.com>
2024-07-16 10:57:27 +0200

    test: Add generate_classification_target and generate_regression_target methods to DatasetDataFrame class

--- tests/datasets/test_datasets_tabular.py
+++ tests/datasets/test_datasets_tabular.py
@@ -5,6 +5,7 @@ import pytest
 
 from siapy.datasets.tabular import DatasetDataFrame, TabularDataEntity
 from siapy.entities import Pixels, SpectralImageSet
+from siapy.models.schemas import ClassificationTarget, RegressionTarget
 
 
 @pytest.fixture(scope="module")
@@ -27,17 +28,17 @@ def sample_dataframe() -> DatasetDataFrame:
     return DatasetDataFrame(df)
 
 
-def test_constructor(sample_dataframe):
+def test_dataframe_constructor(sample_dataframe):
     assert isinstance(sample_dataframe._constructor(), type(sample_dataframe))
 
 
-def test_signals(sample_dataframe):
+def test_dataframe_signals(sample_dataframe):
     signals_df = sample_dataframe.signals()
     assert list(signals_df.columns) == ["0", "1"]
     assert signals_df.equals(pd.DataFrame({"0": [5, 6], "1": [7, 8]}))
 
 
-def test_metadata(sample_dataframe):
+def test_dataframe_metadata(sample_dataframe):
     metadata_df = sample_dataframe.metadata()
     assert list(metadata_df.columns) == [
         "image_idx",
@@ -62,12 +63,53 @@ def test_metadata(sample_dataframe):
     )
 
 
-def test_pixels(sample_dataframe):
+def test_dataframe_pixels(sample_dataframe):
     pixels_df = sample_dataframe.pixels()
     assert list(pixels_df.columns) == [Pixels.coords.U, Pixels.coords.V]
     assert pixels_df.equals(
         pd.DataFrame({Pixels.coords.U: [1, 2], Pixels.coords.V: [3, 4]})
     )
+
+
+def test_dataframe_generate_classification_target_single_column(sample_dataframe):
+    classification_target = sample_dataframe.generate_classification_target(
+        "shape_type"
+    )
+    assert isinstance(classification_target, ClassificationTarget)
+    assert all(
+        classification_target.label
+        == pd.Series([("rectangle",), ("circle",)], name="label")
+    )
+    assert classification_target.value.name == "encoded"
+    assert classification_target.encoding.name == "encoding"
+    assert list(classification_target.value) == [0, 1]
+    assert classification_target.encoding.to_dict() == {
+        0: ("rectangle",),
+        1: ("circle",),
+    }
+
+
+def test_dataframe_generate_classification_target_multiple_columns(sample_dataframe):
+    classification_target = sample_dataframe.generate_classification_target(
+        ["shape_type", "shape_label"]
+    )
+    assert isinstance(classification_target, ClassificationTarget)
+    assert all(
+        classification_target.label
+        == pd.Series([("rectangle", "c"), ("circle", "d")], name="label")
+    )
+    assert list(classification_target.value) == [0, 1]
+    assert classification_target.encoding.to_dict() == {
+        0: ("rectangle", "c"),
+        1: ("circle", "d"),
+    }
+
+
+def test_dataframe_generate_regression_target(sample_dataframe):
+    regression_target = sample_dataframe.generate_regression_target("0")
+    assert isinstance(regression_target, RegressionTarget)
+    assert regression_target.name == "0"
+    assert all(regression_target.value == sample_dataframe["0"])
 
 
 def test_tabular_len(spectral_tabular_dataset):
