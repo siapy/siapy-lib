c414141841d07b3e87a3943cbbb8f4e01ab229f5
janezlapajne <lapajne.janez@gmail.com>
2024-07-24 10:16:32 +0200

    refactor: Add mock_sklearn_dataset fixture for tests

--- tests/utils/test_utils_evaluators.py
+++ tests/utils/test_utils_evaluators.py
@@ -2,7 +2,6 @@ import logging
 
 import pytest
 from sklearn.base import BaseEstimator
-from sklearn.datasets import make_classification
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score, make_scorer
 from sklearn.model_selection import KFold, train_test_split
@@ -45,31 +44,23 @@ def test_check_model_methods_invalid():
         _check_model_methods(model)
 
 
-@pytest.fixture(scope="module")
-def mock_dataset():
-    X, y = make_classification(
-        n_samples=100, n_features=10, n_classes=2, random_state=0
-    )
-    return X, y
-
-
-def test_cross_validation(mock_dataset):
-    X, y = mock_dataset
+def test_cross_validation(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     mean_score = cross_validation(model=SVC(random_state=0), X=X, y=y)
     assert mean_score == pytest.approx(0.9)
     mean_score = cross_validation(model=DummyClassifier(), X=X, y=y)
     assert round(mean_score, 2) == pytest.approx(0.52)
 
 
-def test_cross_validation_with_kfold(mock_dataset):
-    X, y = mock_dataset
+def test_cross_validation_with_kfold(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     kf = KFold(n_splits=5, random_state=0, shuffle=True)
     mean_score = cross_validation(model=SVC(random_state=0), X=X, y=y, cv=kf)
     assert mean_score == pytest.approx(0.92)
 
 
-def test_cross_validation_with_custom_scorer(mock_dataset):
-    X, y = mock_dataset
+def test_cross_validation_with_custom_scorer(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     custom_scorer = make_scorer(accuracy_score)
     mean_score = cross_validation(
         model=SVC(random_state=0), X=X, y=y, scoring=custom_scorer
@@ -77,8 +68,8 @@ def test_cross_validation_with_custom_scorer(mock_dataset):
     assert mean_score == pytest.approx(0.9)
 
 
-def test_cross_validation_with_x_val_y_val(mock_dataset, caplog):
-    X, y = mock_dataset
+def test_cross_validation_with_x_val_y_val(mock_sklearn_dataset, caplog):
+    X, y = mock_sklearn_dataset
     caplog.set_level(logging.INFO)  # Set the logging level to INFO
     mean_score = cross_validation(model=SVC(random_state=0), X=X, y=y, X_val=X)
     assert mean_score == pytest.approx(0.9)
@@ -88,16 +79,16 @@ def test_cross_validation_with_x_val_y_val(mock_dataset, caplog):
     )
 
 
-def test_hold_out_validation(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     score = hold_out_validation(model=SVC(random_state=0), X=X, y=y, random_state=0)
     assert score == pytest.approx(0.95)
     score = hold_out_validation(model=DummyClassifier(), X=X, y=y, random_state=0)
     assert score == pytest.approx(0.40)
 
 
-def test_hold_out_validation_with_manual_validation_set(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation_with_manual_validation_set(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=0
     )
@@ -107,8 +98,10 @@ def test_hold_out_validation_with_manual_validation_set(mock_dataset):
     assert score == pytest.approx(0.95)
 
 
-def test_hold_out_validation_with_incomplete_manual_validation_set(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation_with_incomplete_manual_validation_set(
+    mock_sklearn_dataset,
+):
+    X, y = mock_sklearn_dataset
     X_train, X_val, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=0)
     with pytest.raises(
         ValueError,
@@ -119,8 +112,8 @@ def test_hold_out_validation_with_incomplete_manual_validation_set(mock_dataset)
         )
 
 
-def test_hold_out_validation_with_custom_scorer_func(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation_with_custom_scorer_func(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     custom_scorer = make_scorer(accuracy_score)
     score = hold_out_validation(
         model=SVC(random_state=0), X=X, y=y, scoring=custom_scorer, random_state=0
@@ -128,16 +121,16 @@ def test_hold_out_validation_with_custom_scorer_func(mock_dataset):
     assert score == pytest.approx(0.95)
 
 
-def test_hold_out_validation_with_custom_scorer_str(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation_with_custom_scorer_str(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     score = hold_out_validation(
         model=SVC(random_state=0), X=X, y=y, scoring="accuracy", random_state=0
     )
     assert score == pytest.approx(0.95)
 
 
-def test_hold_out_validation_with_stratify(mock_dataset):
-    X, y = mock_dataset
+def test_hold_out_validation_with_stratify(mock_sklearn_dataset):
+    X, y = mock_sklearn_dataset
     score = hold_out_validation(
         model=SVC(random_state=0), X=X, y=y, stratify=y, random_state=0
     )
