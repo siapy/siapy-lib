8539ef7f3eb1a61c5734ffc5da1212a298006611
janezlapajne <lapajne.janez@gmail.com>
2024-08-01 20:35:38 +0200

    refactor: Rename display_image_with_selected_areas to display_image_with_areas and display_multiple_images_with_selected_areas to display_multiple_images_with_areas; add interactive plot of multiple images

--- tests/utils/test_utils_plots.py
+++ tests/utils/test_utils_plots.py
@@ -1,7 +1,9 @@
 import pytest
 
+from siapy.utils.enums import InteractiveButtonsEnum
 from siapy.utils.plots import (
-    display_image_with_selected_areas,
+    display_image_with_areas,
+    display_multiple_images_with_areas,
     pixels_select_click,
     pixels_select_lasso,
 )
@@ -17,4 +19,22 @@ def test_pixels_select_click_manual(spectral_images):
 def test_pixels_select_lasso_manual(spectral_images):
     image_vnir = spectral_images.vnir
     selected_areas = pixels_select_lasso(image_vnir)
-    display_image_with_selected_areas(image_vnir, selected_areas, color="blue")
+    display_image_with_areas(image_vnir, selected_areas, color="blue")
+
+
+@pytest.mark.manual
+def test_display_multiple_images_with_areas(spectral_images, corresponding_pixels):
+    image_vnir = spectral_images.vnir
+    image_swir = spectral_images.swir
+    selected_areas_vnir = corresponding_pixels.vnir
+    selected_areas_swir = corresponding_pixels.swir
+    # selected_areas_vnir = pixels_select_lasso(image_vnir)
+    # selected_areas_swir = pixels_select_lasso(image_swir)
+    out = display_multiple_images_with_areas(
+        images_with_areas=[
+            (image_vnir, selected_areas_vnir),
+            (image_swir, selected_areas_swir),
+        ],
+        color="blue",
+    )
+    assert isinstance(out, InteractiveButtonsEnum)
